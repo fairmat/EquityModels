5f9c2c4bb92138280d6e2229091066b5db46ae6e
Stefano Angeleri <weltall2@gmail.com>
2012-09-28 11:33:28 +0200

    Added a description to the tests assembly.

--- EquityModels.Tests/Properties/AssemblyInfo.cs
+++ EquityModels.Tests/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EquityModels.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Several test for Equity Models plugins.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fairmat SRL")]
 [assembly: AssemblyProduct("EquityModels.Tests")]
