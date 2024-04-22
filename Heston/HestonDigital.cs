9c1267e89abbada6f39666a03b0854402cb054c1
Giovanni La Cagnina <giovanni.lacagnina@fairmat.com>
2024-04-22 13:02:45 +0200

    [RD-PRICING] creating file for Heston Digital options fairmat/dev/plugins#466

---
+++ Heston/HestonDigital.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HestonEstimator;
+
+namespace Heston
+{
+    internal class HestonDigital: HestonCall
+    {
+        // define the constructors from the base class 
+        public HestonDigital() { }
+
+        public HestonDigital(HestonCallOptimizationProblem problem) : base(problem) { }
+
+        public HestonDigital(HestonProcess process, double strike, double timeToMaturity) : base (process, strike, timeToMaturity) { } 
+
+    }
+}
